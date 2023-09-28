62d48c8a5fa781cf6668430cf37b5d2d416139ed
Stephane Fontaine <esselfe16@gmail.com>
2023-09-28 03:29:47 -0400

    0.2.1: introduce GIF image processing

--- pix2html.h
+++ pix2html.h
@@ -1,9 +1,11 @@
 #ifndef PIX2HTML_H
 #define PIX2HTML_H 1
 
+int GIF_GetSize(char *filename, unsigned int *width,
+	unsigned int *height, unsigned int *pixel_size);
 int JPG_GetSize(char *filename, unsigned int *width,
-    unsigned int *height, unsigned int *pixel_size);
+	unsigned int *height, unsigned int *pixel_size);
 int PNG_GetSize(char *filename, unsigned int *width,
-    unsigned int *height, unsigned int *pixel_size);
+	unsigned int *height, unsigned int *pixel_size);
 
 #endif /* PIX2HTML_H */
